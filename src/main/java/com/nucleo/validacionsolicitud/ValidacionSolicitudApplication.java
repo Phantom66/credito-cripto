8163653f1b17e98fbcfd11e81c398a16c38fba54
Andres Leotur <leotur@develop.develop>
2022-05-11 15:55:57 -0400

    Iniciando proyecto credito

---
+++ src/main/java/com/nucleo/validacionsolicitud/ValidacionSolicitudApplication.java
@@ -0,0 +1,13 @@
+package com.nucleo.validacionsolicitud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidacionSolicitudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValidacionSolicitudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nucleo/validacionsolicitud/ValidacionSolicitudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nucleo.validacionsolicitud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidacionSolicitudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
