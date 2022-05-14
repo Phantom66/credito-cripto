e102e809967cbcc7ea123fd015388f4278e0f030
Andres Leotur <leoturorellana@gmail.com>
2022-05-14 19:01:48 -0400

    Añadiendo trazas con lombok

--- src/main/java/com/nucleo/validacionsolicitud/controller/ClientController.java
+++ src/main/java/com/nucleo/validacionsolicitud/controller/ClientController.java
@@ -2,38 +2,35 @@ package com.nucleo.validacionsolicitud.controller;
 
 import java.util.List;
 
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
 import com.nucleo.validacionsolicitud.model.Client;
 import com.nucleo.validacionsolicitud.service.ClientServiceImpl;
 
 
 
+@Slf4j
 @RequestMapping("/")
 @RestController
 public class ClientController {
 
-
-    private final static Logger LOGGER = LoggerFactory.getLogger(ClientController.class);
     @Autowired
     private ClientServiceImpl clientService;
 
     @GetMapping(value = "obtener-clientes")
     public List<Client> findAll(){
-        LOGGER.info("Obteniendo clientes");
+        log.info("Obteniendo clientes");
         return clientService.findAll();
     }
 
     @PostMapping(value = "guadar-cliente")
     public Client save(@RequestBody Client client){
-        LOGGER.info("Guardando cliente {}", client);
+        log.info("Guardando cliente {}", client);
         return clientService.save(client);
     }
     
