04d6530c9802c615f0cadf9b69ced73ec753d7a0
Andres Leotur <leoturorellana@gmail.com>
2022-05-15 14:28:17 -0400

    ajustando identación IRequestService

--- src/main/java/com/nucleo/validacionsolicitud/service/ResquestService.java
+++ src/main/java/com/nucleo/validacionsolicitud/service/ResquestService.java
@@ -1,17 +1,12 @@
 package com.nucleo.validacionsolicitud.service;
 
-
 import java.util.stream.Stream;
 import com.nucleo.validacionsolicitud.model.Request;
 
-
-
-
-
 public interface ResquestService {
-    
-    public Stream<Request>find();
-    public Request findRequest(String document);
 
+    public Stream<Request> find();
+
+    public Request findRequest(String document);
 
 }
